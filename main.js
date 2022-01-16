719204541aa8efb61f26c69b998c1049e98be01d
AdrianDieDose <AdrianDuda2019@gmail.com>
2022-01-16 19:51:56 +0100

    Added eventHandler and calculations for the overlap.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import "./style.css";
 import * as THREE from "three";
-import { Camera, StringKeyframeTrack } from "three";
+import { Camera } from "three";
 
 let camera, scene, renderer; // ThreeJS globals
 const originalBoxSize = 3; // Originals width and height of a box
+let stack = []; // Height of each layer
+let gameStarted = false;
+const boxHeight = 1; // Height of each layer
+let direction; //Something fishy
 
 function init() {
-  const scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
   // Foundation
   addLayer(0, 0, originalBoxSize, originalBoxSize);
@@ -23,11 +27,11 @@ function init() {
 
   const width = 10;
   const height = width * (window.innerHeight / window.innerWidth);
-  const camera = new THREE.OrthographicCamera(
-    width / -2,
-    width / 2,
-    height / 2,
-    height / -2,
+  camera = new THREE.OrthographicCamera(
+    width / -2, //Left
+    width / 2, //Right
+    height / 2, //Top
+    height / -2, //Bottom
     1,
     100
   );
@@ -35,7 +39,7 @@ function init() {
   camera.position.set(4, 4, 4);
   camera.lookAt(0, 0, 0);
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
 
@@ -43,20 +47,18 @@ function init() {
 }
 
 function addLayer(x, z, width, depth, direction) {
-  const y = boxHeight * StringKeyframeTrack.length; // Add the new box one layer higher
+  const y = boxHeight * stack.length; // Add the new box one layer higher
 
   const layer = generateBox(x, y, z, width, depth);
-  layer.direction = direction;
+  layer.direction = direction; //Something fishy
   stack.push(layer);
 }
 
 function generateBox(x, y, z, width, depth) {
-  const geometry = new THREE.BoxGeometry(width, boxHeight, width);
+  const geometry = new THREE.BoxGeometry(width, boxHeight, depth);
 
-  const color = new THREE.Color(
-    `hsl(${30 + StringKeyframeTrack.length * 4}, 100%, 50%)`
-  );
-  const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+  const color = new THREE.Color(`hsl(${30 + stack.length * 4}, 100%, 50%)`);
+  const material = new THREE.MeshLambertMaterial({ color });
 
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(x, y, z);
@@ -67,5 +69,63 @@ function generateBox(x, y, z, width, depth) {
     threejs: mesh,
     width,
     depth,
+    direction, // Added bc error ?
   };
 }
+
+window.addEventListener("click", () => {
+  if (!gameStarted) {
+    renderer.setAnimationLoop(animation);
+    gameStarted = true;
+  } else {
+    const topLayer = stack[stack.length - 1];
+    const previousLayer = stack[stack.length - 2];
+
+    const direction = topLayer.direction;
+
+    const delta =
+      topLayer.threejs.position[direction] -
+      previousLayer.threejs.position[direction];
+
+    const overhangSize = Math.abs(delta);
+
+    const size = direction == "x" ? topLayer.width : topLayer.depth;
+
+    const overlap = size - overhangSize;
+
+    if (overlap > 0) {
+      //Cut layer
+      const newWidth = direction == "x" ? overlap : topLayer.width;
+      const newDepth = direction == "z" ? overlap : topLayer.depth;
+
+      //Update metadata
+      topLayer.width = newWidth;
+      topLayer.depth = newDepth;
+
+      //Update ThreeJS Model
+      topLayer.threejs.scale[direction] = overlap / size;
+      topLayer.threejs.position[direction] -= delta / 2;
+
+      //Next layer
+      const nextX = direction == "x" ? topLayer.threejs.position.x : -10;
+      const nextZ = direction == "z" ? topLayer.threejs.position.z : -10;
+      const nextDirection = direction == "x" ? "z" : "x";
+
+      addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+    }
+  }
+});
+
+function animation() {
+  const speed = 0.15;
+
+  const topLayer = stack[stack.length - 1];
+  topLayer.threejs.position[topLayer.direction] += speed;
+
+  if (camera.position.y < boxHeight * (stack.length - 2) + 4) {
+    camera.position.y += speed;
+  }
+  renderer.render(scene, camera);
+}
+
+init();
